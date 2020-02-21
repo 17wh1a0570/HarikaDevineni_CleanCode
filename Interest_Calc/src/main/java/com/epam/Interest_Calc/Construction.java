bb22a6b2f21ee87d8745887b881ed6128ee75fb5
17wh1a0570 <17wh1a0570@bvrithyderabad.edu.in>
2020-02-22 00:11:07 +0530

    New java class to calculate construction costs

---
+++ Interest_Calc/src/main/java/com/epam/Interest_Calc/Construction.java
@@ -0,0 +1,41 @@
+package com.epam.Interest_Calc;
+import java.io.*;
+import java.util.Scanner;
+
+public class Construction {
+		public static void cons() {
+			PrintStream p = new PrintStream(new FileOutputStream(FileDescriptor.out));
+			Scanner scan = new Scanner(System.in);
+			p.println("Select the Material: ");
+			p.println("1.Standard Materials");
+			p.println("2.Above Standard Materials");
+			p.println("3.High Standard Materials");
+			p.println("4.High Standard Materials and Fully Automated home");
+			try {
+				int ch = scan.nextInt();
+				double house_area, total_cost = -1;
+				p.println("Enter area of the house");
+				house_area = scan.nextDouble();
+				scan.close();
+				switch(ch) {
+				case 1:
+					total_cost = 1200 * house_area;
+					break;
+				case 2:
+					total_cost = 1500 * house_area;
+					break;				
+				case 3:
+					total_cost = 1800 * house_area;
+					break;
+				case 4:
+					total_cost = 2500 * house_area;
+					break;
+				default:
+					p.println("Please choose from 1-4");
+				}
+				if (total_cost != -1) p.println("Cost of building the house in rupees is : " + total_cost);
+			} catch (Exception e) {
+				p.println("Please enter a valid input");
+			}
+		}
+	}
